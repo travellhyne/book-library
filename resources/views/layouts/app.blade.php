f69dab1c4e9cd7ca841cf5f7af17b14a53bbbb4f
Neil John Ramal <ramalneiljohn@gmail.com>
2016-11-26 12:35:46 +0800

    Rename welcome template to home

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Book Library</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+    </head>
+    <body>
+        @yield('content')
+        <script src="{{ URL::asset('js/app.js') }}"></script>
+    </body>
+</html>
