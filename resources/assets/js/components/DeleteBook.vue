8b95ec786f5fa450f96feaa00e0d886e17a7b956
Neil John Ramal <ramalneiljohn@gmail.com>
2016-11-26 13:10:29 +0800

    Add Capability to delete in client side

--- resources/assets/js/components/DeleteBook.vue
+++ resources/assets/js/components/DeleteBook.vue
@@ -3,18 +3,23 @@
     <h1>Confirm delete?</h1>
     <div class="row">
       <button class="btn btn-default" v-on:click="goBack">No</button>
-      <button class="btn btn-danger" v-on:click="delete">Yes</button>
+      <button class="btn btn-danger" v-on:click="deleteBook">Yes</button>
     </div>
   </div>
 </template>
 
 <script>
   import 'whatwg-fetch';
-  import router from ../router;
+  import router from '../router';
   export default {
     methods: {
       goBack() {
         router.push({name: 'home'});
+      },
+      deleteBook() {
+        const id = this.$route.params.id;
+        fetch('/api/delete/' + id, { method: 'DELETE' })
+          .then(response => router.push({name: 'home'}));
       }
     }
   };
